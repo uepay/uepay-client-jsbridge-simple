5a504b5ae9fdcd67833acad3991e56a4d142ccb1
MattChanDev <ccy4031@qq.com>
2022-06-07 15:02:11 +0800

    创建iOS目录
    创建iOS目录

---
+++ iOS/iOS/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  iOS
+//
+//  Created by uepay-pc-016 on 2022/6/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ iOS/iOS/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  iOS
+//
+//  Created by uepay-pc-016 on 2022/6/7.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

dfeb58f8fcc9b2715cae1fe0cdfee15dcfbf744f
developer-zsg <a793226275@163.com>
2022-06-07 11:26:13 +0800

    增加android客戶端示例代碼

---
+++ android/app/src/main/java/com/uepay/webview/simple/UePayWebViewActivity.java
@@ -0,0 +1,20 @@
+package com.uepay.webview.simple;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by zsg on 2022/6/7.
+ * Desc: UePay客戶端WebView組件SDK使用示例（demo代碼僅做參考）
+ * <p>
+ * Copyright (c) 2022 UePay.mo All rights reserved.
+ */
+public class UePayWebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

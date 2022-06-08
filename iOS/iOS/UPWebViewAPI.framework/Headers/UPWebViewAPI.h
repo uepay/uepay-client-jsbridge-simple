9a8bf05b90e323d87de2f0561f164d1bd87d0a1b
MattChanDev <ccy4031@qq.com>
2022-06-08 16:48:12 +0800

    打包framework
    导入framework

---
+++ iOS/iOS/UPWebViewAPI.framework/Headers/UPWebViewAPI.h
@@ -0,0 +1,24 @@
+//
+//  UPWebViewAPI.h
+//  UPWebViewAPI
+//
+//  Created by uepay-pc-016 on 2022/6/7.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for UPWebViewAPI.
+FOUNDATION_EXPORT double UPWebViewAPIVersionNumber;
+
+//! Project version string for UPWebViewAPI.
+FOUNDATION_EXPORT const unsigned char UPWebViewAPIVersionString[];
+
+
+#import <UPWebViewAPI/UPWebViewManager.h>
+#import <UPWebViewAPI/DWKWebView.h>
+//#import <UPWebViewAPI/UPPayJsApi.h>
+// In this header, you should import all the public headers of your framework using statements like #import <UPWebViewAPI/PublicHeader.h>
+
+
+
